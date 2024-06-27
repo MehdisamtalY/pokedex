fbfc919f7a20bc3ddd0f6305a84e58dac3c7bdc5
MehdisamtalY <mehdi.samtal@hotmail.fr>
2024-06-27 17:05:19 +0200

    fin exercie 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
-
-const PokemonCard = () => {
+const pokemonList  = [
+  {
+    name: 'Pikachu',
+    imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+  },
+  {
+    name: 'Bulbasaur',
+    imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
   
-      return (
-                 <figure>
-     
-                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+];
 
-                 <figcaption> Bulbasaur </figcaption>
-                </figure>
+function PokemonCard() {
+
+const pokemon = pokemonList[0];
+
+  return (
+    <div className="pokemon-card">
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      <p>???</p>
+    </div>
   );
-};
+}
 
 export default PokemonCard;
+
