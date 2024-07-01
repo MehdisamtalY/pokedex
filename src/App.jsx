674e5b2c77ae83162dadb57d6edbe0caac894699
MehdisamtalY <mehdi.samtal@hotmail.fr>
2024-07-01 23:41:36 +0200

    basics 06 fin

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,73 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard';
 
-const pokemonList  = [
+const pokemonList = [
   {
-    name: 'Pikachu',
-    imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: 'Bulbasaur',
-    imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   
 ];
 
-
 function App() {
+
   const [pokemonIndex, setPokemonIndex] = useState(0);
+  
+  
+  
+  const handlePrevClick = () => {
+    setPokemonIndex((prevIndex) => (prevIndex === 0 ? pokemonList.length - 1 : prevIndex - 1));
+  console.log(pokemonIndex)
+  };
 
-      const handleClick = (event) => {
-      console.log(event.target)
 
-      return (
-        <div>
-          <button onClick={handlePrevious} disabled={pokemonIndex === 0}>Précédent</button>
-          <button onClick={handleNext} disabled={pokemonIndex === pokemonList.length - 1}>Suivant</button>
-        </div>
-      );
-      
-    }
+  const handleNextClick = () => {
+    setPokemonIndex((prevIndex) => (prevIndex === pokemonList.length - 1 ? 0 : prevIndex + 1));
+  };
 
-    const handlePrevious = () => {
-      setPokemonIndex((prevIndex) => prevIndex - 1);
-    };
-    
-    const handleNext = () => {
-      setPokemonIndex((prevIndex) => prevIndex + 1);
-    };
-   
+  let button; let button2;
+  if (pokemonIndex < pokemonList.length -1) {
+    button = <button onClick={handleNextClick}>Suivant </button>
+    if (pokemonIndex != 0){
+       button2 =<button onClick={handlePrevClick}>Précédent</button>
+      }
   
 
+  } else {
+    
+      button2 =<button onClick={handlePrevClick}>Précédent</button>
+    
+    
+  }
+ 
+
   return (
-    <PokemonCard pokemon = {pokemonList[1]} />
+    <div>
+      <img src={pokemonList[pokemonIndex].imgSrc} alt={pokemonList[pokemonIndex].name} />
+      <div>
+      {button2} 
+       {button}
+      </div>
+    </div>
   );
 }
 
 
+
 export default App;
-
-
